eef85df2fc91754540b06cc8380e7cb4a873455b
Phil <filippo.troiani96@gmail.com>
2022-01-25 02:24:12 +0100

    2021 17 dic

--- 2021/17.py
+++ 2021/17.py
@@ -138,7 +138,7 @@
 INPUT_PATH = './input/17.txt'
 import re
 
-class TargetArea:
+class TargetArea:   # I used it only for testing
     def __init__(self, x1, x2, y1, y2):
         self.x1 = x1
         self.x2 = x2
